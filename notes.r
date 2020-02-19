86aba5e1b5ea26707cf7d37a69c8f91edc93387a
olliemcdonald <ollie.mcdonald17@gmail.com>
2020-02-19 11:52:25 -0500

    first commit

---
+++ notes.r
@@ -0,0 +1,31 @@
+# Contents -------------------------------------
+# - Intro to R and RStudio
+#   - Using Help
+#   - Data types
+#       - numeric, char, factor
+#       - vectors, matrix, array, list, dataframe
+#       - str, mode, summary
+#   - Basic Operations
+#   - Working with Data
+#       - Importing data
+#       - Data Manipulation (introduce tidyverse)
+#   - Libraries
+# - Working With Data
+#   - Statistics Operations, Using Functions
+#   - Writing Functions
+#   - Plotting
+# - Statistics and Modeling
+#   - Statistics and Summary Info
+#   - Regression
+#   - Survival Analysis
+# - More info
+#   - Websites for more information
+#     - Rstudio, hadley,
+#   - References
+
+
+# Briefly show Rstudio
+
+?sum
+help(sum)
+help.search("sum")
